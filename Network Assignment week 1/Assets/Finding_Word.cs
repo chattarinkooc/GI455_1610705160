a8e4c98f8969211b089c8721550375b126adf642
DESKTOP-D48B35N\CKK <chattarin.kooc@bumail.net>
2021-01-27 23:07:42 +0700

    Send Assignment Week1

---
+++ Network Assignment week 1/Assets/Finding_Word.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Finding_Word : MonoBehaviour
+{
+    public string findWord;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void Names()
+    {
+
+        findWord = inputField.GetComponent<Text>().text;
+
+        if (findWord == "Football")
+        {
+            textDisplay.GetComponent<Text>().text = " ["+ findWord +"] "+"is found ";
+        }
+        else if (findWord == "Basketball")
+        {
+            textDisplay.GetComponent<Text>().text = " ["+ findWord +"] "+"is found ";
+        }
+        else if (findWord == "Golf")
+        {
+            textDisplay.GetComponent<Text>().text = " ["+ findWord +"] "+"is found ";
+        }
+        else if (findWord == "Tennis")
+        {
+            textDisplay.GetComponent<Text>().text = " ["+ findWord +"] "+"is found ";
+        }
+        else if (findWord == "Handball")
+        {
+            textDisplay.GetComponent<Text>().text = " ["+ findWord +"] "+"is found ";
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = " ["+ findWord +"] "+"not found ";
+        }
+
+
+    }
+}
+ 
